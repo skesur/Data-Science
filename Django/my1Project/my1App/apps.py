cd425f2be2a496ac94a59f00c67b8a98d43adcb3
Saumya Kesur <saumyakesur@gmail.com>
2026-05-26 19:46:18 +0530

    Delete Django directory

--- Django/my1Project/my1App/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class My1AppConfig(AppConfig):
-    name = 'my1App'
--- Django/my2Project/my2App/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class My2AppConfig(AppConfig):
-    name = 'my2App'
